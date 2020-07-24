ec3789d81b4aa6e7c9d21a323b22c45cbab90ba1
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-07-24 22:08:00 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -26,8 +26,10 @@ class School
   end
 
   def sort
-    @roster.each {|grade| @roster.fetch(grade).sort}
+    @roster.each do |grade| 
+      @roster.fetch(grade).sort
     binding.pry
+  end
     @roster
     #sorted_roster = @roster[grade]
     #binding.pry
